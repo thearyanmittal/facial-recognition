bf348f74419e7340e44490fac08143d44da5ccbb
Aryan Mittal <aaryanm2005@gmail.com>
2021-02-08 11:49:59 -0500

    changed model url (ResNet V2) added progress bar for transfer model loading

--- face_train.py
+++ face_train.py
@@ -11,6 +11,7 @@ sns.set()
 import tensorflow as tf #for advanced classification
 import tensorflow_hub as hub
 from tensorflow.keras import layers
+os.environ['TFHUB_DOWNLOAD_PROGRESS'] = "1" #progress bar for KerasLayer creating model
 
 #"walk" through files, looking for .jpg, .jpeg, or .png and add them to list of training objects
 
@@ -155,7 +156,7 @@ tf_train = normalize(tf_train) #normalize all the augmented data
 #the classifier that will be used is Resnet 50
 #use feature_vector version, not classification; feature_vector has last layer removed so it's customizable
 
-URL = "https://tfhub.dev/tensorflow/resnet_50/feature_vector/1" #url of the model
+URL = "https://tfhub.dev/google/imagenet/resnet_v2_50/feature_vector/4" #url of the model
 resnet = hub.KerasLayer(URL, trainable=False) #don't train existing parameters
 
 #build the Sequential model (put in the ResNet feature vector, then add my output layer with appropriate number of neurons)
